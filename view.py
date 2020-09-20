84bc616298b5e732d2ce30565a01944819d55e66
Andrey Cherkasov <andreychas1@gmail.com>
2020-09-21 02:54:44 +0300

    added sample display to the MainView

--- view.py
+++ view.py
@@ -21,7 +21,16 @@ class MainView(tk.Toplevel):
         self.label = tk.Label(self.top_frame, text=LABEL_TEXT)
         self.label.pack(side='top', anchor='w')
 
+        self.sample_list = tk.Listbox(self.top_frame, selectmode='extended')
+        self.sample_list.pack(side='top', anchor='w')
+
         self.bot_frame = tk.Frame(self)
         self.bot_frame.pack(side='bottom', expand=True, fill='both')
         if debugging():
             self.bot_frame.config(bg='blue')
+
+
+    def display_samples(self, samples):
+        self.sample_list.delete(0, tk.END)
+        for sample in samples:
+            self.sample_list.insert(tk.END, sample)
