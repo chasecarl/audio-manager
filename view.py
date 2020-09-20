86b2cfdaf121aa5a4cc920a6ea4c29723405587f
Andrey Cherkasov <andreychas1@gmail.com>
2020-09-21 02:36:10 +0300

    added MainView

--- view.py
+++ view.py
@@ -1,10 +1,27 @@
 import tkinter as tk
+from util import debugging
 
 
 TITLE = 'Audio Manager'
+LABEL_TEXT = 'My Database'
 
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.title(TITLE)
-    root.mainloop()
+class MainView(tk.Toplevel):
+
+    def __init__(self, master):
+        tk.Toplevel.__init__(self, master)
+        self.protocol('WM_DELETE_WINDOW', self.master.destroy)
+        self.title(TITLE)
+        
+        self.top_frame = tk.Frame(self)
+        self.top_frame.pack(side='top', expand=True, fill='both')
+        if debugging():
+            self.top_frame.config(bg='red')
+
+        self.label = tk.Label(self.top_frame, text=LABEL_TEXT)
+        self.label.pack(side='top', anchor='w')
+
+        self.bot_frame = tk.Frame(self)
+        self.bot_frame.pack(side='bottom', expand=True, fill='both')
+        if debugging():
+            self.bot_frame.config(bg='blue')
