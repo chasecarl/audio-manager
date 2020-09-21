e3229636b84c196c6d35e405e3ce43b7a64f15b3
Andrey Cherkasov <andreychas1@gmail.com>
2020-09-21 12:19:39 +0300

    changed default debug level

--- util.py
+++ util.py
@@ -5,11 +5,11 @@ import logging
 ENV_DEBUG_VARNAME = 'AMDEBUG'
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def debugging():
     result = os.environ.get(ENV_DEBUG_VARNAME) is not None
     if result:
-        logging.info(f'Debug check: {result}.')
+        logging.debug(f'Debug check: {result}.')
     return result
