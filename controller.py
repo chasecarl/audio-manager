033aa5ab73f2eeb15e8d7b2ac54d152825e9bb40
Andrey Cherkasov <andreychas1@gmail.com>
2020-10-08 18:00:30 +0300

    added dialog_* methods that manage appropriate views (if present) and perform model calls

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import logging
 
 
@@ -13,63 +14,58 @@ class Controller:
         self.model = RawTextAudioCollection()
         self.view = MainView(root)
         self.model.add_callback(self.model_changed)
-        self.view.add_callback(self.selection_changed)
-        self.view.display_samples(self.model)
-        self.view.add_button.config(command=self.add_entry)
-        self.view.remove_button.config(command=self.remove_entry)
-        self.view.rename_button.config(command=self.rename_entry)
-        self.view.concat_button.config(command=self.concat_audio)
-
-
-    def selection_changed(self, **kwargs):
-        selection = kwargs[SELECTION]
-        logging.debug(f'C: Passing the following selection to the model: {str(selection)}')
-        self.model.set_selected(selection)
-
-
-    def entry_added(self, **kwargs):
-        name = kwargs[ENTRY_NAME]
-        path = kwargs[ENTRY_PATH]
-        # FIXME sometimes the callback is still there on the next iteration
-        self.view.remove_callback(self.entry_added)
-        logging.debug(f'C: Passing the following entry to the model: {name}, {path}')
-        self.model.add_entry(name, path)
-
-
-    def entry_renamed(self, **kwargs):
-        name = kwargs[ENTRY_NAME]
-        self.view.remove_callback(self.entry_renamed)
-        logging.debug(f'C: Passing the following entry name to the model: {name}')
-        self.model.rename_entry(name)
-
-
-    def audio_saved(self, **kwargs):
-        audio_filename = kwargs[AUDIO_FILENAME]
-        self.view.remove_callback(self.audio_saved)
-        logging.debug(f'C: Passing the following audio filename to the model: {audio_filename}')
-        self.model.concat_audio(audio_filename)
-
+        self.view.display(self.model)
+        self.view.add_button.config(command=self.dialog_add_entry)
+        self.view.remove_button.config(command=self.dialog_remove_entry)
+        self.view.rename_button.config(command=self.dialog_rename_entry)
+        self.view.concat_button.config(command=self.dialog_concat_audio)
+        self.current_dialog = None
 
     def model_changed(self):
-        self.view.display_samples(self.model)
+        self.view.display(self.model)
 
+    def _dispose_current_dialog(self):
+        if not self.current_dialog:
+            return
+        self.current_dialog.destroy()
+        self.current_dialog = None
+
+    def dialog_add_entry(self):
+        self.current_dialog = AddView(self.view)
+        self.current_dialog.accept_button.config(command=self.add_entry)
+        self.current_dialog.grab_set()
 
     def add_entry(self):
-        self.view.add_callback(self.entry_added)
-        self.view.add_entry_dialog()
+        # TODO validate
+        self.model.add((self.current_dialog.name_entry.get(), ), (self.current_dialog.path_entry.get(), ))
+        self._dispose_current_dialog()
 
+    def dialog_remove_entry(self):
+        answer = messagebox.askokcancel(
+            title='Confirm removal',
+            message='Are you sure to remove selected items?',
+            icon='warning'
+        )
+        if answer:
+            self.remove_entries()
 
-    def remove_entry(self):
-        self.model.remove_entry()
+    def remove_entries(self):
+        # TODO validate
+        selection = self.model.names_selected()
+        self.model.remove(selection)
 
+    def dialog_rename_entry(self):
+        self.current_dialog = RenameView(self.view)
+        self.current_dialog.accept_button.config(command=self.rename_entry)
+        self.current_dialog.grab_set()
 
     def rename_entry(self):
-        self.view.add_callback(self.entry_renamed)
-        self.view.rename_entry_dialog()
+        # TODO validate
+        selection = self.model.names_selected()
+        self.model.rename(selection[0], self.current_dialog.name_entry.get())
+        self._dispose_current_dialog()
 
-
-    def concat_audio(self):
-        self.view.add_callback(self.audio_saved)
+    def dialog_concat_audio(self):
         self.view.save_concat_audio_dialog()
 
 
